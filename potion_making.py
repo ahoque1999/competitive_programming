f789259e6c11fd2962f9ffd8798f6dca6699d13a
ahoque1999 <ayhoque@gmail.com>
2022-04-03 02:23:27 +0800

    potion making 800

---
+++ potion_making.py
@@ -0,0 +1,12 @@
+def gcd(x, y):
+    y = max([x, y])
+    x = min([x, y])
+    
+    while y:
+        x, y = y, x % y
+    return x
+
+for _ in range(int(input())):
+    k = int(input())
+
+    print(100 // gcd(k, 100))
