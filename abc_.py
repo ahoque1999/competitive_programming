e8d23b1b69df1311e2418eafe437e67f03895dce
ahoque1999 <ayhoque@gmail.com>
2022-04-05 12:35:26 +0800

    removed old abc.py

--- abc_.py
+++
@@ -1,9 +0,0 @@
-for iteration in range(int(input())):
-    n = int(input())
-    s = input()
-
-    if s == "11" or s == "00":
-        print("NO")
-        continue
-
-    print("YES") if n <= 2 else print("NO")
